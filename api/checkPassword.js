f65bb9c40113d25a2e89244d35307b76b21e2c7d
Havish Karthikeya Kanamarlapudi <havishkanamarlapudi@gmail.com>
2025-10-26 07:52:16 +0530

    Update checkPassword.js

--- api/checkPassword.js
+++ api/checkPassword.js
@@ -1,9 +1,8 @@
 export default function handler(req, res) {
-  const { password } = req.body;
-
-  if (password === "BOEING787") {
+  const { password } = req.query;
+  if (password === 'BOEING787') {
     res.status(200).json({ success: true });
   } else {
-    res.status(200).json({ success: false, message: "Access Denied 🚫" });
+    res.status(403).json({ success: false });
   }
 }
