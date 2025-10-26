a2adc51cc9b045eebea25a3a0092c344c659644d
Havish Karthikeya Kanamarlapudi <havishkanamarlapudi@gmail.com>
2025-10-26 07:54:50 +0530

    Update getSecret.js

--- api/getSecret.js
+++ api/getSecret.js
@@ -1,3 +1,4 @@
 export default function handler(req, res) {
-  res.status(200).json({ success: true, message: "FLEET" });
+  const secret = "FLEET";
+  res.status(200).json({ success: true, secret });
 }
